0ab494316a0ab49ffe1e931c54d4e3c3fcbaae3e
Coder <294ryan@gmail.com>
2026-04-24 14:19:29 +0800

    內置pyinstall跟python.exe 改了main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ import tkinter.messagebox as tkm
 from threading import Thread
 
 def resourcePath(relative_path):
-    # 獲取資源的絕對路徑 兼容開發環境與 PyInstaller
     try:
-        # PyInstaller 建立的臨時資料夾路徑存放在 _MEIPASS 中
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
@@ -23,64 +21,33 @@ def showInfo(msg):
 def showError(msg):
     print(f"[Error] {msg}")
 
-def autoInstallPip():
-    try:
-        pipScript = resourcePath("get-pip.py")
-        # 使用目前的 Python 解釋器執行該腳本
-        result = subprocess.run(
-            [sys.executable, pipScript], 
-            check=True, 
-            text=True)
-        showInfo("Pip auto installed successful.")
-        showInfo(f"Output: {result.stdout[:50]}...")
-        # 重新自動安裝pyinstaller
-        autoInstallPyinstaller()
-    except subprocess.CalledProcessError as e:
-        showError(f"Pip install failed: {e.stderr}")
-        tkm.showerror("Unexpected error", f"Error while installing pip. Error message: {e.stderr} / Error code: {e.returncode}. Please try again.")
-
-def autoInstallPyinstaller():
+def runCommand(commandList: list):  # return tuple: (isSuccessful, returnCode)
     try:
         result = subprocess.run(
-            ["pip", "install", "pyinstaller"], 
+            commandList, 
             capture_output=True,    # 擷取輸出文字
-            text=True,    # 將輸出轉換為字串 而非bytes
-            check=True    # 失敗時拋出異常
+            text=True,    # 將輸出轉換為字串
+            check=True    # 執行有問題會raise Error
         )
-        showInfo("Pyinstaller installed successful.")
-        showInfo(f"Output: {result.stdout[:50]}...")
-        # 測試pyinstaller可不可以用
-        testPyinstaller()
+        
+        showInfo("Executed successfully")
+        showInfo(f"Output: {result.stdout}")
+        return True, 0
 
     except subprocess.CalledProcessError as e:
         showError(f"Execution failed, error code: {e.returncode}")
         showError(f"Error message: {e.stderr}")
-        tkm.showerror("Unexpected error", f"Error while installing pyinstaller. Error message: {e.stderr} / Error code: {e.returncode}. Please try again.")
-
-    except FileNotFoundError:
-        showInfo("Pip could not be found, trying auto install...")
-        autoInstallPip()
+        return False, e.returncode
+    except Exception as e:
+        showError("Execution failed with a unexcepted error")
+        showError(str(e))
+        return False, -1
 
 def testPyinstaller():
-    try:
-        result = subprocess.run(
-            ["pyinstaller", "--version"], 
-            capture_output=True,    # 擷取輸出文字
-            text=True,    # 將輸出轉換為字串 而非bytes
-            check=True    # 失敗時拋出異常
-        )
-        showInfo("Pyinstaller ran successful.")
-        showInfo(f"Output: {result.stdout[:50]}...")
-    
-    except subprocess.CalledProcessError as e:
-        showError(f"Pyinstaller ran failed, error code: {e.returncode}")
-        showError(f"Error message: {e.stderr}")
-        tkm.showerror("Unexpected error", f"Unexpected error while testing Pyinstaller.Error message: {e.stderr} / Error code: {e.returncode}. Please try again.")
-    
-    except FileNotFoundError:
-        # 找不到pyinstall 試用pip自動安裝
-        autoInstallPyinstaller()
-
+    showInfo("Testing Pyinstaller...")
+    isSuccessful, _ = runCommand([resourcePath(r"python_core/python.exe"), "-m", "pyinstaller", "--version"])
+    if isSuccessful:
+        showInfo("Pyinstaller tested successfully")
 
 def main():
     testPyinstaller()
