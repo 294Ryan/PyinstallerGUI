ee31d093b047c41af6e0903bc5441dd3f6682a7e
Coder <294ryan@gmail.com>
2026-04-27 14:55:50 +0800

    可以正常test pyinstaller

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import subprocess
 import sys
 import os
-'''
+
 import tkinter as tk
 import tkinter.messagebox as tkm
-'''
+
 from threading import Thread
 
 def resourcePath(relative_path):
@@ -27,7 +27,8 @@ def runCommand(commandList: list):  # return tuple: (isSuccessful, returnCode)
             commandList, 
             capture_output=True,    # 擷取輸出文字
             text=True,    # 將輸出轉換為字串
-            check=True    # 執行有問題會raise Error
+            check=True,   # 執行有問題會raise Error
+            env = ENV    # 自行注入環境變數
         )
         
         showInfo("Executed successfully")
@@ -36,7 +37,7 @@ def runCommand(commandList: list):  # return tuple: (isSuccessful, returnCode)
 
     except subprocess.CalledProcessError as e:
         showError(f"Execution failed, error code: {e.returncode}")
-        showError(f"Error message: {e.stderr}")
+        showError(f"Error message: {e.stderr if e.stderr else e.stdout}")
         return False, e.returncode
     except Exception as e:
         showError("Execution failed with a unexcepted error")
@@ -45,15 +46,22 @@ def runCommand(commandList: list):  # return tuple: (isSuccessful, returnCode)
 
 def testPyinstaller():
     showInfo("Testing Pyinstaller...")
-    isSuccessful, _ = runCommand([PYINSTALLER_PATH, "--version"])
+    isSuccessful, _ = runCommand([PYTHON_PATH, "-m", "PyInstaller", "--version"])
     if isSuccessful:
         showInfo("Pyinstaller tested successfully")
 
 def main():
     testPyinstaller()
 
-
+PYTHON_PATH = resourcePath(r"python_core/python.exe")
 PYINSTALLER_PATH = resourcePath(r"python_core/Scripts/pyinstaller.exe")
+CORE_PATH = resourcePath("python_core")
+LIB_PATH = os.path.join(CORE_PATH, "Lib", "site-packages")
+
+# 建立自定義環境變數
+ENV = os.environ.copy()
+ENV["PATH"] = CORE_PATH + os.pathsep + ENV.get("PATH", "")  # 確保PATH包含python_core 讓pyinstaller.exe可以找到python313.dll
+ENV["PYTHONPATH"] = LIB_PATH    # 確保PYTHONPATH 包含site-packages
 
 
 if __name__ == "__main__":
